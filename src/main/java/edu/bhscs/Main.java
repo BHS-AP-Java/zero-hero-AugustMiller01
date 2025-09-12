3785d413d9aa33878b958dfdbd3064c0683dfb69
AugustM <2019302@apps.nsd.org>
2025-09-12 16:46:35 +0000

    using print / falling asleep

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,21 @@
-// Student name
-// class period
-// project name
-// date
+// August Miller
+// P2
+// Zero-Hero
+// 9/12
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr.Reiber says
+ * INPUT: we don't know about input yet
+ * OUTPUT:Some cool words inthe command line terminal thing
+ * EDGE CASE:THey are all edge cases
  */
 
 package edu.bhscs;
+
+class Main{
+  public static void main(String[] args){
+
+    System.out.println("Hello World");
+
+  }
+}
