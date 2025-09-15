4ac3d31cc88808e765be89f57a1c6312e6a09d1b
AugustMiller01 <2019302@apps.nsd.org>
2025-09-15 09:48:09 -0700

    I am learning about morality

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -13,13 +13,44 @@
 package edu.bhscs;
 
 class Main {
-  public static void main(String[] args) {
 
-    System.out.println("Hello World!");
-    System.out.println("  0/ ");
-    System.out.println(" /| ");
-    System.out.println("Goodbye World!");
-    System.out.println("  0/ ");
-    System.out.println(" /| ");
+  public static final char A = 'a';
+  /*To make this project mine i tested out if I could create
+  my own variable like A that would work the same way*/
+
+  public static final char B = 'b';
+
+  public static void main(String[] args) {
+    int someNumber = 67;
+    //test
+
+    System.out.println("A's");
+    System.out.println("------------------------------------");
+    System.out.println();
+    System.out.println("67!" + A + someNumber);
+    System.out.println(A + "?" + A + someNumber);
+
+    /*This is the most interesting like, as for all previous examples
+    A functions as just a character, the letter a. But here,
+    it can add itself with other numbers as long as there
+    are no strings*/
+    System.out.println(A + A + someNumber);
+    System.out.println();
+
+    System.out.println();
+
+    System.out.println("B's");
+    System.out.println("------------------------------------");
+    System.out.println();
+    System.out.println("67!" + B + someNumber);
+    System.out.println(B + "?" + B + someNumber);
+    System.out.println(B + B + someNumber);
+    System.out.println("this is neat");
+    System.out.println("------------------------------------");
+    System.out.println("");
+    System.out.println(B + A);
+    System.out.println("");
+
+    System.out.println("Ok Im Done");
   }
 }
