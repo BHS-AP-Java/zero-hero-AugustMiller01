ad80c67dbdef222e51a1d90354fb721f0e918d77
AugustM <2019302@apps.nsd.org>
2025-09-22 16:34:27 +0000

    please be cake

---
+++ src/main/java/edu/bhscs/Cake.java
@@ -0,0 +1,26 @@
+package edu.bhscs;
+
+public class Cake {
+  String ingredient;
+  String frosting;
+  int weight;
+  public Cake(String ingredient,String frosting){
+    this.ingredient = ingredient;
+    this.frosting = frosting;
+    this.weight = 10;
+    System.out.println("ooo a "+ ingredient + " cake with " + frosting + " frosting!");
+
+
+  }
+  public void eatCake(int hunger, String name){
+    System.out.println();
+    this.weight -= hunger;
+    System.out.println(name + " ate " + hunger + " slices of the " + ingredient + " cake with " + frosting + " frosting");
+
+
+  }
+  public void getWeight(){
+    System.out.println("the " + ingredient + " cake with " + frosting + " frosting has "+ weight +" slices left;");
+  }
+
+}
