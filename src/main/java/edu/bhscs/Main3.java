b5b5b65028f2b668858dc02b2ea8ddf9a99af46a
AugustM <2019302@apps.nsd.org>
2025-10-06 16:14:20 +0000

    more stuff

--- src/main/java/edu/bhscs/Main3.java
+++ src/main/java/edu/bhscs/Main3.java
@@ -1,15 +1,10 @@
 package edu.bhscs;
 
-import java.util.Scanner;
-
 public class Main3 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("What can we do with a scanner?");
-    System.out.println("What is your favorite color?");
-    String color = scanner.next();
-    System.out.println("idk but im done now!!!");
 
-    scanner.close();
+  public static void main(String[] args) {
+
+    Player myPlayer = new Player("August");
+    myPlayer.answerQuestions();
   }
 }
--- src/main/java/edu/bhscs/Player.java
+++ src/main/java/edu/bhscs/Player.java
@@ -1,1 +1,19 @@
-public class Player {}
+package edu.bhscs;
+
+import java.util.Scanner;
+
+public class Player {
+  String name;
+  Scanner scanner = new Scanner(System.in);
+
+  public Player(String name) {
+    this.name = name;
+    this.scanner = new Scanner(System.in);
+  }
+
+  public void answerQuestions() {
+    System.out.println("what's your question?");
+    String question = this.scanner.next();
+    System.out.println(question + "? thats a stupid question.");
+  }
+}
